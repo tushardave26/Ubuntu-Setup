c9be930937a01f48a96e5571a77b7b55b57c3af6
tushardave26@gmail.com <tushardave26@gmail.com>
2019-12-10 22:18:37 -0500

    Add script to setup basic ubuntu packages

---
+++ ubuntu_setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# update and upgrade the Ubuntu
+sudo apt update
+sudo apt upgrade
+
+# install media codecs
+sudo apt -y install ubuntu-restricted-extras
+
+# install tweal tool
+sudo apt -y install gnome-tweak-tool
+
+# install tlp and tlp-dw for prolong your battery and prevent overheating
+sudo apt -y install tlp tlp-rdw
+sudo tlp start
+
+# system cleanup
+sudo apt autoremove
+
+exit;
