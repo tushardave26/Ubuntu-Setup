84a47eecc9409e1af50b81617103df12301a0e70
Tushar Dave <tushardave26@gmail.com>
2019-12-17 16:30:52 -0500

    Add docker installation steps to developer essential script

--- developer_essentials.sh
+++ developer_essentials.sh
@@ -54,6 +54,19 @@ sudo apt -y install mysql-server mysql-client libmysqlclient-dev mysql-workbench
 # PostgreSQL
 # sudo apt -y install postgresql-11 libpq-dev
 
+# install Docker
+## add the GPG key for the official Docker repository to the system
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+## add the Docker repository to APT sources
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+## update the package database with the Docker packages from the newly added repo
+sudo apt update
+
+## install Docker
+sudo apt install docker-ce
+
 # system cleanup
 sudo apt autoremove
 
